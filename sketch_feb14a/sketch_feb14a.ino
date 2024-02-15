7f2124ea6457ba06f1a28b73d583d2d4bb3e7edb
SamMcp14 <19068@orewacollege.nz>
2024-02-16 11:57:10 +1300

    Update sketch_feb14a.ino

--- sketch_feb14a/sketch_feb14a.ino
+++ sketch_feb14a/sketch_feb14a.ino
@@ -3,18 +3,23 @@
 const byte MICPIN = 8;
 const byte SERVOPIN = 10;
 const byte LEDPIN = 5;
+const byte BTNPIN = 4;
 
 Servo boxServo;
+
 void setup() {
-pinMode(MICPIN, INPUT)
-pinMode(SERVOPIN, OUTPUT)
-lock.attach(SERVOPIN);
+  pinMode(MICPIN, INPUT);
+  pinMode(SERVOPIN, OUTPUT);
+  boxServo.attach(SERVOPIN);
 }
 
 void loop() {
-if analogRead(MICPIN) <= 250) [
-digitalWrite(LEDPIN, HIGH)
-
-]
+  if (analogRead(MICPIN) >= 250) {
+    digitalWrite(LEDPIN, HIGH);
+    boxServo.write(90);
+  }
+  if (analogRead(BTNPIN) == HIGH) {
+    boxServo.write(0);
+    digitalWrite(LEDPIN, LOW);
+  }
 }
-
