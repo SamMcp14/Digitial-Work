d061e7d951d177732c4835dbdb86919139b900c4
SamMcp14 <19068@orewacollege.nz>
2024-02-23 13:16:51 +1300

    Update sketch_feb20a.ino

--- sketch_feb20a/sketch_feb20a.ino
+++ sketch_feb20a/sketch_feb20a.ino
@@ -1,17 +1,65 @@
-const byte BTNPIN = 1
-const byte POTPIN = A1
-void setup() {
-pinMode(BTNPIN, INPUT);
-pinMode(POTPIN, INPUT);
+#include <LiquidCrystal.h>
 
-Serial.begin(9600)
+const byte BTNPIN = 1
+const byte NOIPIN = A0
+const byte POTPIN = A1
+
+
+  Adafruit_LiquidCrystal screen(0);
+
+void setup() {
+  pinMode(BTNPIN, INPUT);
+  pinMode(POTPIN, INPUT);
+
+  randomSeed(analogRead(NOIPIN));
+
+  Serial.begin(9600)
+
+    if (!screen.begin(16, 2)) {
+    Serial.println(check wiring didnt start) while (1);
+  }
 }
 
 void loop() {
 if (digitalRead(BTNPIN == HIGH) {
-  Serial.println("rolling dice! YIPPEE");
-  int diceType = analogRead(POTPIN);
-diceType = map(diceType, 0, 1023, 1. 6);
-rollDice(diceType);
+    Serial.println("rolling dice! YIPPEE");
+    int diceType = analogRead(POTPIN);
+    diceType = map(diceType, 0, 1023, 1. 6);
+    rollDice(diceType);
+  }
 }
+
+void rollDice(byte numSides) {
+  byte diceRoll = 255;
+  switch (numSides) [
+    case 1:
+    screen.print("roll is: ")
+    diceRoll = Random(1, 4);
+    break;
+        case 2:
+    screen.print("roll is: ")
+    diceRoll = Random(1, 6);
+    break;
+        case 3:
+    screen.print("roll is: ")
+    diceRoll = Random(1, 8);
+    break;
+        case 4:
+    screen.print("roll is: ")
+    diceRoll = Random(1, 10);
+    break;
+        case 5:
+    screen.print("roll is: ")
+    diceRoll = Random(1, 12);
+    break;
+        case 6:
+    screen.print("roll is: ")
+    diceRoll = Random(1, 20);
+    break;
+    default:
+    Serial.println("Issue with mapping potentiometer values")
+    breal;
+  ]
 }
+
+screen.println(diceRoll);
