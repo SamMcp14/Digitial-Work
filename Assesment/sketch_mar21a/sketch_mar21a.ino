35f69d624f6f8a5c4452656533a357c7e60aeb0d
SamMcp14 <19068@orewacollege.nz>
2024-04-05 12:37:25 +1300

    Update sketch_mar21a.ino

--- Assesment/sketch_mar21a/sketch_mar21a.ino
+++ Assesment/sketch_mar21a/sketch_mar21a.ino
@@ -3,29 +3,30 @@
 #include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
 #include <SparkFun_LPS25HB_Arduino_Library.h>
 
-//
-LPS25HB Sensor; // Create an object of the LPS25HB class
-OpenLog resultStorage; //Create instance
+
+//,, 
+LPS25HB Sensor;
+OpenLog resultStorage;
 
 void setup()
 {
-    Serial.begin(9600);
- 
+  Serial.begin(9600);
+
   Wire.begin();
-  resultStorage.begin(); //Open connection to OpenLog (no pun intended)
+  resultStorage.begin();
 
-  Sensor.begin(); // Begin links an I2C port and I2C address to the sensor, sets an I2C speed, begins I2C on the main board, and then sets default settings
+  Sensor.begin();
 
-  if (Sensor.isConnected() == false) // The library supports some different error codes such as "DISCONNECTED"
+  if (Sensor.isConnected() == false)
   {
-    Serial.println("LPS25HB disconnected. Reset the board to try again.");     // Alert the user that the device cannot be reached
-  
+    Serial.println("BOARD HAS RECONECTED");
+
     while (1);
-  }
- 
+  } 
+
   resultStorage.append("resultStorage.txt");
   resultStorage.println("This is recorded to resultStorage.txt");
- 
+
   resultStorage.syncFile();
 
   Serial.println("Done!");
@@ -35,17 +36,17 @@ void loop()
 {
   Serial.print("Pressure in hPa: ");
   resultStorage.print("Pressure in hPa: ");
-  Serial.print(Sensor.getPressure_hPa()); // Get the pressure reading in hPa
-  resultStorage.print(Sensor.getPressure_hPa()); // Get the pressure reading in hPa
+  Serial.print(Sensor.getPressure_hPa());
+  resultStorage.print(Sensor.getPressure_hPa());
   resultStorage.println("               ");
   Serial.println("               ");
   Serial.print(", Temperature (degC): ");
   resultStorage.print(", Temperature (degC): ");
-  Serial.println(Sensor.getTemperature_degC()); // Get the temperature in degrees C
-  resultStorage.println(Sensor.getTemperature_degC()); // Get the temperature in degrees C
+  Serial.println(Sensor.getTemperature_degC()); 
+  resultStorage.println(Sensor.getTemperature_degC()); 
   resultStorage.println("               ");
   Serial.println("               ");
 
   resultStorage.syncFile();
-    delay(1000); // Wait - 40 ms corresponds to the maximum update rate of the sensor (25 Hz)
+  delay(1000);
 }
