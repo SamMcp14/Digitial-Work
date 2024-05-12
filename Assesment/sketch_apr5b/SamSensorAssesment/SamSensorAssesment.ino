4c3cf08f82a549bd3938d9831843bc67b04296bb
SamMcp14 <19068@orewacollege.nz>
2024-05-13 10:16:38 +1200

    Monday Morning

--- Assesment/sketch_apr5b/SamSensorAssesment/SamSensorAssesment.ino
+++ Assesment/sketch_apr5b/SamSensorAssesment/SamSensorAssesment.ino
@@ -20,26 +20,47 @@ void setup() {
   //TEACHER COMMENT what is this checking?
   if (Sensor.isConnected() == false) {
     Serial.println("SENSORBOARD HAS FAILED TO CONNECT");
-
-    while (1);
+    while (1)
+      ;
   }
 
   //TEACHER COMMENT should you also check the sdcard/resultStorage?
 
   resultStorage.append("resultStorage.txt");
-resultStorage.print("Time,Pressure,Temprature");
+  resultStorage.print("Time,Pressure,Temprature");
 
   resultStorage.syncFile();
 
-  Serial.println("Done!");
-
-Serial.print("Time,Pressure,Temperature");
+  Serial.println("SETUP IS DONE");
 }
 
 //TEACHER COMMENT add a comment explaining what this is doing
 void loop() {
-  myTime = millis();
-  Serial.print(myTime); // prints time since program 
+ 
+  // if (Sensor.getPressure_hPa() >= 0) {
+  //     digitalWrite(myLed, HIGH);
+  //     delay(10);
+  //     digitalWrite(myLed, LOW);
+  // }
+  // else{
+  //   Serial.print("Pressure sensor not recording proper data");
+  // }
+  // if (Sensor.getTemperature_degC() >= 0) {
+  //     digitalWrite(myLed, HIGH);
+  //     delay(10);
+  //     digitalWrite(myLed, LOW);
+  // }
+  // else{
+  //   Serial.print("Temprature sensor not recording proper data");
+  // }
+  //   delay(1000);
+  writeToSDcard(true);  //t = debugging  f = rocket lauch
+}
+
+void writeToSDcard(bool debug) {
+  if (debug) {
+ myTime = millis();
+  Serial.print(myTime);  // prints time since program
   resultStorage.print(myTime);
   Serial.print(",");
   resultStorage.print(",");
@@ -51,21 +72,19 @@ void loop() {
   resultStorage.println(Sensor.getTemperature_degC());
 
   resultStorage.syncFile();
-if (Sensor.getPressure_hPa() >= 0) {
-    digitalWrite(myLed, HIGH);
-    delay(10);
-    digitalWrite(myLed, LOW);
-}
-else{
-  Serial.print("Pressure sensor not recording proper data");
-}
-if (Sensor.getTemperature_degC() >= 0) {
-    digitalWrite(myLed, HIGH);
-    delay(10);
-    digitalWrite(myLed, LOW);
-}
-else{
-  Serial.print("Temprature sensor not recording proper data");
-}
-  delay(1000);
+  } else {
+ myTime = millis();
+ // Serial.print(myTime);  // prints time since program
+  resultStorage.print(myTime);
+ // Serial.print(",");
+  resultStorage.print(",");
+ // Serial.print(Sensor.getPressure_hPa());
+  resultStorage.print(Sensor.getPressure_hPa());
+ // Serial.print(",");
+  resultStorage.print(",");
+ // Serial.println(Sensor.getTemperature_degC());
+  resultStorage.println(Sensor.getTemperature_degC());
+
+  resultStorage.syncFile();
+  }
 }
