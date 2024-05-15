dd5b75fd08cbfc4911240d8dd2e7d6f883c249bb
SamMcp14 <19068@orewacollege.nz>
2024-05-15 13:56:17 +1200

    comments

--- Assesment/sketch_apr5b/SamSensorAssesment/SamSensorAssesment.ino
+++ Assesment/sketch_apr5b/SamSensorAssesment/SamSensorAssesment.ino
@@ -1,89 +1,72 @@
+// Include the external libaries used.
 #include <Wire.h>
 #include <SparkFun_Qwiic_OpenLog_Arduino_Library.h>
 #include <SparkFun_LPS25HB_Arduino_Library.h>
 
-//TEACHER COMMENT what is this being created for?
-long myTime;
-const String FILE_NAME ="ResultStorage.txt";
+long myTime; // creating a variable for the amount of time passed since code started
+const String FILE_NAME ="ResultStorage.txt"; // created a const for the file name
 
 //TEACHER COMMENT so what are these?
-LPS25HB Sensor;
-OpenLog resultStorage;
+LPS25HB Sensor; // Create the object of the presure sensor
+OpenLog resultStorage; // Create instance
 
 void setup() {
-  Serial.begin(9600);
-  Wire.begin();
-  resultStorage.begin();
-  Sensor.begin();
+  Serial.begin(9600); // start serial monitor
+  Wire.begin(); // Initalizes wire libary
+  resultStorage.begin(); // Starts sd card storage
+  Sensor.begin(); // starts the pressure/temprature sensor
 
-  //TEACHER COMMENT what is this checking?
+// If sensor isnt connect print error message 
   if (Sensor.isConnected() == false) {
     Serial.println("SENSORBOARD HAS FAILED TO CONNECT");
-    while (1)
-      ;
+    while (1);
   }
 
   //TEACHER COMMENT should you also check the sdcard/resultStorage?
 
-  resultStorage.append(FILE_NAME);
-  resultStorage.print("Time,Pressure,Temprature");
+  resultStorage.append(FILE_NAME); // Make the file for results on sdcard
+  resultStorage.print("Time,Pressure,Temprature"); // CSV format
 
-  resultStorage.syncFile();
+  resultStorage.syncFile(); 
 
-  Serial.println("SETUP IS DONE");
+  Serial.println("SETUP IS DONE"); // start loop
 }
 
 //TEACHER COMMENT add a comment explaining what this is doing
 void loop() {
-
-  // if (Sensor.getPressure_hPa() >= 0) {
-  //     digitalWrite(myLed, HIGH);
-  //     delay(10);
-  //     digitalWrite(myLed, LOW);
-  // }
-  // else{
-  //   Serial.print("Pressure sensor not recording proper data");
-  // }
-  // if (Sensor.getTemperature_degC() >= 0) {
-  //     digitalWrite(myLed, HIGH);
-  //     delay(10);
-  //     digitalWrite(myLed, LOW);
-  // }
-  // else{
-  //   Serial.print("Temprature sensor not recording proper data");
-  // }
-  //   delay(1000);
   writeToSDcard(true);  //t = debugging  f = rocket lauch
 }
 
 void writeToSDcard(bool debug) {
   if (debug) {
     myTime = millis();
-    Serial.print(myTime);  // prints time since program
+    // prints time since program
+    Serial.print(myTime);
     resultStorage.print(myTime);
-    Serial.print(",");
+// print comma
+    Serial.print(","); 
     resultStorage.print(",");
+// print pressure from sensor
     Serial.print(Sensor.getPressure_hPa());
     resultStorage.print(Sensor.getPressure_hPa());
+// print comma
     Serial.print(",");
     resultStorage.print(",");
+// print temprature
     Serial.println(Sensor.getTemperature_degC());
     resultStorage.println(Sensor.getTemperature_degC());
 
     resultStorage.syncFile();
-  } else {
+// sink sdcard file
+  } else { // if not debuging just print to resultStorage
     myTime = millis();
-    // Serial.print(myTime);  // prints time since program
     resultStorage.println(myTime);
-    // Serial.print(",");
     resultStorage.print(",");
-    // Serial.print(Sensor.getPressure_hPa());
     resultStorage.println(Sensor.getPressure_hPa());
-    // Serial.print(",");
     resultStorage.print(",");
-    // Serial.println(Sensor.getTemperature_degC());
-    resultStorage.println(Sensor.getTemperature_degC());
+      resultStorage.println(Sensor.getTemperature_degC());
 
     resultStorage.syncFile();
+// syncFile
   }
 }
